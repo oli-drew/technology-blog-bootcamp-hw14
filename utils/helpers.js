7a2a4959539b79b599555b933317ac13e28b36fd
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-31 01:43:32 +0100

    Homepage view updated and helpers created

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,18 @@ module.exports = {
     // Format date to local format
     return date.toLocaleDateString();
   },
+  is_even: (num) => {
+    if (num % 2 == 0) {
+      return true;
+    }
+  },
+  is_odd: (num) => {
+    if (num % 2 != 0) {
+      return true;
+    }
+  },
+  make_substring: (str) => {
+    if (str.length > 100) return `${str.substring(0, 100)}...`;
+    return str;
+  },
 };
