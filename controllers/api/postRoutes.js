e407a2beff0ffaadd8a5b599048e4a5f14901f16
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-04-01 15:24:55 +0100

    setup backend for comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,6 +15,25 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
+// Update a blog post by id
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: "No blog post found with this id!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.delete("/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
